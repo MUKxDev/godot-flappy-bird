ca4b39c44f4f809c4cc6419e19c0d0b07a88b617
Mohamed Mukhtar <mukxdev@gmail.com>
2023-10-30 10:05:34 +0300

    finished game

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,5 +1,7 @@
 extends Node
 
+@export var pipe_scene : PackedScene
+
 var game_running: bool = false
 var game_over : bool= false
 var score
@@ -9,13 +11,15 @@ var screen_size: Vector2i
 var ground_height: int
 var pipes : Array
 const PIPE_DELAY: int = 100
-const PIPE_RANGE: int = 200
+const PIPE_RANGE: int = 60
 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_window().size
+	var ground_rect = $Ground.get_node("TileMap").get_used_rect()
+	ground_height = ground_rect.size.x
 	new_game()
 	
 
@@ -24,6 +28,12 @@ func new_game():
 	game_over= false
 	score = 0
 	scroll = 0
+	$ScoreLabel.text = 'SCORE: ' + str(score)
+	get_tree().call_group('pipes','queue_free')
+	$GameOver.hide()
+	pipes.clear()
+	#get first pipe
+	generate_pipes()
 	$Bird.reset()
 	
 
@@ -36,6 +46,7 @@ func _input(event):
 				else:
 					if $Bird.flying:
 						$Bird.flap()
+						check_top()
 
 
 func start_game():
@@ -43,6 +54,7 @@ func start_game():
 	game_running = true
 	$Bird.flying = true
 	$Bird.flap()
+	$PipeTimer.start()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -52,3 +64,49 @@ func _process(delta):
 		if scroll >= screen_size.x:
 			scroll = 0
 		$Ground.position.x = -scroll
+		for pipe in pipes:
+			pipe.position.x -= SCROLL_SPEED
+
+
+func _on_pipe_timer_timeout():
+	generate_pipes()
+	
+	
+func generate_pipes():
+	var pipe = pipe_scene.instantiate()
+	pipe.position.x = screen_size.x + PIPE_DELAY
+	pipe.position.y = (screen_size.y - ground_height) / 2 + randi_range(-PIPE_RANGE, PIPE_RANGE)
+	pipe.hit.connect(bird_hit)
+	pipe.scored.connect(bird_scored)
+	add_child(pipe)
+	pipes.append(pipe)
+
+func check_top():
+	if $Bird.position.y < 0:
+		$Bird.falling = true
+		stop_game()
+		
+func stop_game():
+	$PipeTimer.stop()
+	$GameOver.show()
+	$Bird.flying = false
+	game_running= false
+	game_over = true
+	
+
+func bird_hit():
+	$Bird.falling = true
+	stop_game()
+	
+func bird_scored():
+	score += 1
+	$ScoreLabel.text = 'SCORE: ' + str(score)
+
+
+func _on_ground_hit():
+	$Bird.falling = false
+	stop_game()
+
+
+func _on_game_over_restart():
+	new_game()
